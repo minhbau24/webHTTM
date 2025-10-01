32688713e851deb270d9902afff89b0cc0a62f81
Minh Bau <luuminhbau2k4@gmail.com>
2025-10-01 21:44:21 +0700

    Refactor voice registration endpoint to improve model loading and embedding extraction; update function parameters and enhance error handling

--- service-voice/api/voice_register.py
+++ service-voice/api/voice_register.py
@@ -2,22 +2,27 @@ from fastapi import APIRouter, UploadFile
 from core.model_loader import ModelLoader
 from utils.audio_preprocess import preprocess_audio
 from utils.feature_extraction import extract_embedding
-import torch
 
 router = APIRouter()
 
 @router.post("/register_voice/")
-async def register_voice(model_name: str, ckpt_path: str, file: UploadFile):
+async def register_voice(model_name: str, ckpt_path: str, file_path: str):
+    """
+    Đăng ký giọng nói mới bằng cách trích xuất embedding từ file audio.
+    Args:
+        model_name: tên model để xác định config
+        ckpt_path: đường dẫn checkpoint của model
+        file_path: đường dẫn file audio
+    Returns:
+        embs: list các tensor embedding (nếu audio dài thì có thể có nhiều hơn 1 embedding)
+    """
     # load model
-    if model_name.lower() not in ["cnn_rnn", "ecapa", "wavlm"]:
-        return {"error": "Model not supported. Choose from ['cnn_rnn', 'ecapa', 'wavlm']"}
-    try:
-        model = ModelLoader(model_name=model_name, num_classes=300, ckpt_path=ckpt_path)
-    except Exception as e:
-        return {"error": f"Error loading model: {e}"}
+    model, device = ModelLoader(model_name=model_name, num_classes=100, ckpt_path=ckpt_path)
+    
+    # preprocess audio
+    features = preprocess_audio(model_name=model_name, file_path=file_path)
 
-    features = preprocess_audio(model_name=model_name, file_path=file.file)
-
-    embs = [extract_embedding(model, f.unsqueeze(0), lengths=torch.tensor([len(f)])) for f in features]
+    # extract embeddings
+    embs = extract_embedding(model=model, features=features, device=device, mean_embedding=False)
 
     return {"embeddings": embs}
